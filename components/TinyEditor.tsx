b4e55ee18221e2d5636dbdb0682b0854e2226316
Md Ashik <ashik@innospace.tech>
2024-05-05 19:35:23 +0600

    tinymce katex view add

--- components/TinyEditor.tsx
+++ components/TinyEditor.tsx
@@ -1,30 +1,48 @@
 'use client'
 
-import React, {useEffect, useRef} from 'react';
+import React, {useRef} from 'react';
 import {Editor} from '@tinymce/tinymce-react';
+import katex from 'katex';
+import 'katex/dist/katex.min.css'
+
 
 interface TinyEditorProps {
     initialValue?: string;
     onEditorChange: (content: string) => void;
 }
 
-export default function TinyEditorRef({ initialValue, onEditorChange }: TinyEditorProps) {
+export default function TinyEditorRef({initialValue, onEditorChange}: TinyEditorProps) {
     const editorRef = useRef<any>(null);
 
-    const onUserPast = (e: any) => {
-        console.log('onUserPast:: ', e)
-    }
+    const onUserPaste = (e: any) => {
+        const clipboardData = e.clipboardData;
+        const pastedData = clipboardData.getData('Text');
+
+        // Patterns for inline and display math
+        const bracketDisplayPattern = /\\\[[^\]]+\\\]/g;
+
+        let newContent = pastedData;
+
+        newContent = newContent.replace(bracketDisplayPattern, match => {
+            const equation = match.slice(2, -2).trim();
+
+            console.log('equation::', equation)
+            return `<div class="katex">${katex.renderToString(equation, {
+                throwOnError: false,
+                displayMode: true,
+            })}</div>`;
+        });
+
+        console.log('FINAL:: ', newContent);
+
+        editorRef.current?.execCommand('mceInsertContent', false, newContent);
+        e.preventDefault();
+    };
+
 
     const onChangeContent = (content: any) => {
         onEditorChange(editorRef.current?.getContent() || '')
-        console.log('onChangeContent:: ', editorRef.current?.getContent())
     }
-
-    const log = () => {
-        if (editorRef.current) {
-            console.log(editorRef.current?.getContent());
-        }
-    };
 
     return (
         <>
@@ -38,16 +56,13 @@ export default function TinyEditorRef({ initialValue, onEditorChange }: TinyEdit
                     height: 350,
                     menubar: false,
                     plugins: [
-                        'lists', 'image', 'charmap', 'code', 'fullscreen', 'table', 'media '
+                        'lists', 'image', 'charmap', 'code', 'fullscreen', 'table', 'media'
                     ],
                     toolbar: 'undo redo | bold italic | alignleft aligncenter | table image ' +
                         'alignright alignjustify | bullist numlist outdent indent | removeformat',
                     content_style: 'body { font-family: Helvetica, Arial, sans-serif; font-size:14px }'
                 }}
-                onPaste={onUserPast} onChange={onChangeContent}/>
-            <p>----------------------</p>
-            <p>Change it:: </p>
-            <button onClick={log}>Log editor content</button>
+                onPaste={onUserPaste} onChange={onChangeContent}/>
         </>
     );
 }
