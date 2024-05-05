5c085873088d30cb444659e4f1f39bffdaa5976e
Md Ashik <ashik@innospace.tech>
2024-05-05 17:50:47 +0600

    tinymce self hosted added with full intregrated

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,25 @@
 
 'use client';
 
-import { useRef, useState } from 'react';
-import Editor, { TinyEditorRef } from '../components/Editor';
+import {useState} from 'react';
+import TinyEditorRef from "@/components/TinyEditor";
 
 export default function Home() {
-  const editorRef = useRef<TinyEditorRef>(null);
-  const [editorContent, setEditorContent] = useState('');
+    const [editorContent, setEditorContent] = useState('<p>sdjk gduigfvd58gv4df 5g6df</p>');
 
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">TinyMCE with Next.js</h1>
-      <Editor ref={editorRef} initialValue="<p>Hello, TinyMCE!</p>" onEditorChange={(content) => setEditorContent(content)} />
-      <h2 className="text-xl font-bold mt-4">Editor Content:</h2>
-      <div className="border p-4" dangerouslySetInnerHTML={{ __html: editorContent }} />
-    </div>
-  );
+    const onChangeEditor = (content: string) => {
+        setEditorContent(content);
+    }
+
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold mb-4">TinyMCE with Next.js</h1>
+            <TinyEditorRef initialValue={'<p>sdjk gduigfvd58gv4df 5g6df</p>'}
+                           onEditorChange={onChangeEditor}
+            />
+            <h2 className="text-xl font-bold mt-4">Editor Content:</h2>
+            <div className="border p-4" dangerouslySetInnerHTML={{__html: editorContent}}></div>
+        </div>
+
+    );
 }
